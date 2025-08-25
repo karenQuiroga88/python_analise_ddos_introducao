088809941a6f643f00f06589bdaa6934ad1f5b6d
karenQuiroga88 <115047409+karenQuiroga88@users.noreply.github.com>
2025-08-25 07:28:39 -0300

    Update 07 - idade.py

--- 07 - idade.py
+++ 07 - idade.py
@@ -7,3 +7,22 @@ while executar :
   opcao = input('\nDeseja testar novamente? \nSim ou não?')
   if opcao == "Não" or "N" or "Nao":
     executar = False
+
+
+### outra forma
+from datetime import date
+executar = True 
+while executar: 
+    anoNasc = int(input('Em que ano você nasceu?'))
+    anoAtual = date.today().year
+    idade = anoAtual - anoNasc 
+    print ('Você tem: ' + str(idade) + 'anos')
+
+    while True: 
+        opcao = input('\nDeseja testar novamente? (Sim ou Não)')
+        if opcao in ['Sim', 'sim']:
+            executar = True
+            break
+        if opcao in ['Não', 'n', 'não']:
+            executar = False
+            break 
